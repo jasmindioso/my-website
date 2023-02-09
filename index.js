7d6c773115eed15e7d7ab2919a6b4ec1c0790dca
jasmindioso <124751659+jasmindioso@users.noreply.github.com>
2023-02-09 14:44:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-navLinks.forEach(link => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    document.body.classList.remove('nav-open');
-    const page = link.getAttribute('href');
-    window.location = page;
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
   });
-});
+}
+
+if (navLinks) {
+  navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      document.body.classList.remove('nav-open');
+      const page = link.getAttribute('href');
+      console.log(page);
+      window.open(page, '_self');
+    });
+  });
+}
+With these checks in place, your code should be able to run without errors, even if the navToggle or navLinks objects are not defined.
+
+
+
