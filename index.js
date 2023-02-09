615cec93c27e5bfded24ec28665a228c974ed451
jasmindioso <124751659+jasmindioso@users.noreply.github.com>
2023-02-09 14:05:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
-
-window.onload = function() {
-  navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-  });
-};
-
 navLinks.forEach(link => {
   link.addEventListener('click', (e) => {
     e.preventDefault();
     document.body.classList.remove('nav-open');
     const page = link.getAttribute('href');
-    console.log(page);
-    window.open(page, '_self');
+    window.location = page;
   });
 });
